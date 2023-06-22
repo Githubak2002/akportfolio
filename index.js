96a1b8c2da9a4855c31b86717cee57acadff3322
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-06-22 23:01:00 +0530

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// hamburger = document.querySe1ector(".hamburger");
+// hamburger. onclick = function() {
+//     navBar = document. querySe1ector(".centernav");
+//     navBar. classList .toggle("active");
+// }
+
+
+// ----------------------------------- Setup and start animation! ----------------------------------- 
+        // var typed = new Typed('#element', {
+        //     strings: ['WEB DEVLOPER','DESIGNER','CODER','3D web DESIGNER','3D ARTIST'],
+        //     typeSpeed: 40,
+        //     backSpeed: 30,
+        //     loop:true,
+        //     // backDelay:1500
+        // });
